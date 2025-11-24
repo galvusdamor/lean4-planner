e6350facff308887ca9254543f62b04945566245
Galvus Damor <galvusdamor@gmail.com>
2025-11-24 15:12:01 +0100

    Add dfs to imports for lake build

--- Graphlib.lean
+++ Graphlib.lean
@@ -1,4 +1,5 @@
 -- This module serves as the root of the `Graphlib` library.
 -- Import modules here that should be built as part of the library.
 import Graphlib.Basic
+import Graphlib.DFS2
 import Graphlib.Planning
