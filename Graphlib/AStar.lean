805eab906f33e8c160662160fd5c2b66a7f8a034
Galvus Damor <galvusdamor@gmail.com>
2026-03-12 01:10:58 +0100

    Global structure of A* optimality proof added
    * requires multiple minor proofs (have-sorries)
    * requires two complicated invars to be proven

--- Graphlib/AStar.lean
+++ Graphlib/AStar.lean
@@ -34,6 +34,10 @@ namespace NatGraph
 
 open WeightedDiGraph
 
+/-- a heuristic is admissible iff for all nodes, the true cost is greater or equal to the heuristic --/
+abbrev admissible (heur : V → ℕ) (goal : V) :=
+  ∀ v : V, g.cost_ge v goal (heur v)
+
 variable (heur : V → ℕ)
 
 def astar (start : V) (goal : V): Option (g.Path start goal) :=
@@ -65,8 +69,222 @@ theorem astar_is_complete (start : V) (goal : V):
   · apply hsearch_expand_keeps_goal_on_stack
   · apply hsearch_expand_goal_becomes_visited_puts_it_on_stack
 
+
+
+
+/-- Optimality proof --/
+/- stack is sorted by the path_order (i.e. distance) value -/
+
+abbrev astar_stack_sorted (s : WeightedDiGraph.base_search_state g (ℕ×ℕ)) :=
+  List.Pairwise (fun u v =>
+    (add_heur u (s.pathOrder u) heur = add_heur v (s.pathOrder v) heur) ∨ (add_heur u (s.pathOrder u) heur ≺ add_heur v (s.pathOrder v) heur)) s.stack
+
+
+
+abbrev node_open (s : WeightedDiGraph.base_search_state g (ℕ×ℕ)) (v : V) :=
+  v ∈ s.stack
+
+abbrev node_closed (s : WeightedDiGraph.base_search_state g (ℕ×ℕ)) (v : V) :=
+  v ∈ s.visited ∧ v ∉ s.stack
+
+
+/-- Invar propose by Hart et al, 1968 (lemma 1):
+    any non-closed node v (i.e. any node that is either on the stack or not visited)
+    for all optimal paths p from start to it
+    there is an open node v' on p for which its current known distance from start is the optimum -/
+abbrev astar_invar (start : V) (s : WeightedDiGraph.base_search_state g (ℕ×ℕ)) :=
+  ∀ v : V, ¬ (node_closed s v) → ∀ p : g.Path start v, p.is_cheapest →
+    ∃ v' ∈ p.support, (node_open s v') ∧ g.cost_is start v' (s.pathOrder v').1 
+
+abbrev astar_path_invar (start : V) (goal : V) (s : WeightedDiGraph.base_search_state g (ℕ×ℕ)) :=
+  --∀ p : g.Path start goal, p.is_cheapest → ∃ v' ∈ p.support, (node_open s v')
+  ∀ p : g.Path start goal, ∃ v' ∈ p.support, (node_open s v')
+
+
+omit [DecidableEq V] in
+lemma astar_invar_holds_at_init (start : V):
+  astar_invar start (WeightedDiGraph.base_search_state_initial (G:=g) start (0,0)) := by
+  unfold astar_invar
+  unfold WeightedDiGraph.base_search_state_initial
+  simp_all
+  intro v a nodup cheapest
+  use start
+  constructor
+  · simp
+  · unfold node_open
+    simp
+    apply cost_v_v
+
+
+
+
+section
+variable (state : WeightedDiGraph.base_search_state g (ℕ×ℕ))
+
+lemma astar_expand_keeps_stack_sorted (goal : V)
+    :
+     ∀ head : V, ∀ tail : List V, 
+        astar_stack_sorted heur state
+          ∧ head ≠ goal
+          ∧ state.stack = head :: tail
+        → astar_stack_sorted  heur (hsearch_step_expand heur state head tail) := by
+      intro head tail ⟨ prior_invar,head_ne_goal,compose⟩ 
+      unfold astar_stack_sorted
+      unfold hsearch_step_expand
+      apply merge_two_prop 
+      · intro a b c a_b b_c
+        apply hsearch_merge_trans
+        · apply a_b
+        · apply b_c
+      · intro a b
+        apply hsearch_merge_total
+      · ext x y
+        rw [FValueComp.lt_B_eq]
+        simp
+
+
+lemma astar_expand_keeps_path_invar (goal : V)
+    (on_stack_or_nei_visited : search_invar_on_stack_or_all_neighbours_visited state)
+    :
+     ∀ head : V, ∀ tail : List V, 
+        astar_path_invar start goal state
+          ∧ head ≠ goal
+          ∧ state.stack = head :: tail
+        → astar_path_invar start goal (hsearch_step_expand heur state head tail) := by
+    intro head tail ⟨ prior_invar, head_ne_goal, stack_compose ⟩
+    unfold astar_path_invar at prior_invar ⊢
+    intro p
+    specialize prior_invar p
+    obtain ⟨v', v'_in_p, was_open ⟩ := prior_invar
+    unfold node_open at was_open ⊢
+    rw [stack_compose] at was_open
+    cases was_open
+    case head =>
+      have head_visited : head ∈ state.visited := by sorry
+      obtain ⟨start_head, head_goal, compose⟩ := p.val.split_at v'_in_p
+      have head_goal_nodup : head_goal.support.Nodup := by sorry
+
+
+      -- TODO: more complicated, we have to run through the path at the next time -- or equivalently, we have to start at the next node
+      --obtain ⟨head',nil_path,adj_head_u',u'_v,supp1,supp2,compose⟩  := Path.recompose ⟨ head_goal, head_goal_nodup⟩ (u:=head) (by simp) head_ne_goal 
+
+      cases head_goal
+      case nil =>
+        contradiction -- (goal ≠ goal)
+      case cons head' adj_head_head' head'_goal =>
+        sorry
+        --have run_path := run_path_through_state_yields_node_on_stack_or_all_visited head goal (Ne.symm head_ne_goal) ⟨head_goal, head_goal_nodup⟩ state head_visited on_stack_or_nei_visited
+        --
+        --cases run_path
+        --case inl first_open =>
+        --  obtain ⟨ u, u_in_support, u_on_stack, u_not_goal, prop ⟩ := first_open
+        --  use u
+        --  constructor
+        --  · sorry -- follows as subpath of u_in_support
+        --  · sorry
+        --case inr all_not_on_stack =>
+        --  sorry
+    case tail v'_in_tail =>
+      use v'
+      constructor
+      · exact v'_in_p 
+      · unfold hsearch_step_expand
+        simp
+        left
+        exact v'_in_tail
+
+lemma astar_expand_keeps_main_invar (goal : V)
+    :
+     ∀ head : V, ∀ tail : List V, 
+        astar_invar start state
+          ∧ head ≠ goal
+          ∧ state.stack = head :: tail
+        → astar_invar start (hsearch_step_expand heur state head tail) := by
+    sorry
+
+end
+
+omit [DecidableEq V] in 
+lemma walk_more_costly_than_chapest (start v : V) (d : ℕ)
+(v_cost_is : g.cost_is start v d) 
+(w : g.Walk start v) :
+    d ≤ w.cost := by
+    unfold cost_is at v_cost_is
+    obtain ⟨p,p_cost,p_cheapest⟩ := v_cost_is
+    rw [←p_cost]
+    unfold Path.is_cheapest at p_cheapest
+    obtain ⟨w', w'_cheaper ⟩ := w.cheaper_path_exists
+    apply le_trans
+    · exact p_cheapest w'
+    · exact w'_cheaper
+
+
+lemma astar_open_node_with_lower_f (start goal : V) (s : WeightedDiGraph.base_search_state g (ℕ×ℕ))
+  (is_admissible : g.admissible heur goal)
+  -- invariant: goal has not been removed ... I.e. every path still has a node on the stack
+  -- this is a strange invariant as the expansion will actually break it!
+  -- actually not! as we only have to prove the invar iff head ≠ goal
+  (has_astar_invar : astar_invar start s)
+  (has_path_invar : astar_path_invar start goal s)
+  :
+  ∀ p : g.Path start goal, p.is_cheapest → 
+    ∃ v' ∈ p.support, (node_open s v') ∧ (s.pathOrder v').1 + (heur v') ≤ p.cost
+  := by
+  intro p p_cheapest
+  unfold astar_invar at has_astar_invar
+  unfold astar_path_invar at has_path_invar
+  obtain ⟨v', v'_in_p, v'_open⟩ := has_path_invar p
+
+  specialize has_astar_invar v'
+  unfold node_closed at has_astar_invar
+  simp [v'_open] at has_astar_invar
+  obtain ⟨start_v', v'_goal, compose⟩ := p.val.split_at v'_in_p
+  have start_v'_nodup : start_v'.support.Nodup := by sorry -- from subpath
+  -- Theorem: subpaths of cheapest paths are cheapest
+  -- needs the subpath argument for nodup
+  have start_v'_cheapest : Path.is_cheapest ⟨start_v', start_v'_nodup⟩ := by sorry
+  
+  specialize has_astar_invar start_v' start_v'_nodup start_v'_cheapest
+
+  obtain ⟨v'',v''_in_support,v''_open,v''_cost_is⟩ := has_astar_invar 
+
+  use v''
+  and_intros
+  · unfold Path.support
+    rw [←compose]
+    rw [Walk.support_of_append]
+    rw [List.mem_append]
+    left
+    exact v''_in_support
+  · exact v''_open
+  · unfold Path.cost 
+    have v''_in_p : v'' ∈ p.support := by sorry 
+    obtain ⟨start_v'', v''_goal, compose⟩ := p.val.split_at v''_in_p
+
+    rw [←compose]
+    rw [Walk.append_cost]
+    apply add_le_add
+    · apply walk_more_costly_than_chapest
+      exact v''_cost_is
+    · unfold admissible at is_admissible
+      specialize is_admissible v''
+      unfold cost_ge at is_admissible
+  
+      -- subwalk of p
+      have v''_goal_nodup : v''_goal.support.Nodup := by sorry
+      specialize is_admissible ⟨v''_goal, v''_goal_nodup⟩ 
+      apply is_admissible
+
+
+
+@[simp]
+theorem admissible_heur_zero_for_goal
+    (is_admissible : g.admissible heur goal):
+    heur goal = 0 := by sorry
+
 /- -/
 theorem astar_is_optimal (start : V) (goal : V)
+    (is_admissible : g.admissible heur goal)
     (returned_path : Option.isSome (astar (g:=g) heur start goal)):
     ((astar (g:=g) heur start goal).get returned_path).is_cheapest := by
     let final : WeightedDiGraph.base_search_state g (ℕ×ℕ) × Bool := WeightedDiGraph.search_with_stack_step (goal:=goal) (start_state := WeightedDiGraph.base_search_state_initial start (0,0)) (hsearch_step_expand heur) (hsearch_expand_metric_reduction heur)
@@ -136,7 +354,61 @@ theorem astar_is_optimal (start : V) (goal : V)
       · rfl
       · apply WeightedDiGraph.base_invar_carries_over_stack_step
         apply hsearch_expand_keeps_base_invars
+    
+    have prop := hsearch_path_extracted_not_longer_than_path_order start final_state t_1 t_2 t_3 (by sorry)
 
-    sorry
+    have has_astar_invar : astar_invar start final_state := by sorry
+    have xx := astar_open_node_with_lower_f heur start goal final_state is_admissible has_astar_invar
+
+
+    apply Path.sufficient_cheapest_path_cheaper
+
+    --unfold Path.is_cheapest
+    intro p' p'_cheapest
+
+    specialize xx p' p'_cheapest
+
+    obtain ⟨v', v'_on_p', v'_open,cost⟩ := xx 
+
+    apply le_trans ; rotate_left
+    · apply cost
+    · clear cost
+      unfold astar search_exe_with_stack_step search_exe
+      simp
+      specialize prop goal h_3
+      apply le_trans
+      · apply prop
+      · -- from invariant
+        have final_stack_sorted : astar_stack_sorted heur final_state := by sorry
+        unfold astar_stack_sorted at final_stack_sorted
+        unfold search_prop_stack_head_is_goal at h_4 
+        apply List.head?_eq_some_iff.mp at h_4
+        obtain ⟨tail, compose⟩ := h_4
+        unfold node_open at v'_open
+        rw [compose] at final_stack_sorted v'_open
+        rw [List.pairwise_cons] at final_stack_sorted
+        obtain ⟨tail_larger, _⟩ := final_stack_sorted
+        cases v'_open
+        · apply Nat.le_add_right
+        · next v'_in_tail =>
+          specialize tail_larger v' v'_in_tail
+          unfold add_heur at tail_larger
+          rw [admissible_heur_zero_for_goal heur is_admissible] at tail_larger
+          simp only [add_zero] at tail_larger
+          cases tail_larger
+          case inl h =>
+            apply le_of_eq
+            exact (Prod.mk_inj.mp h).1
+          case inr h => 
+            unfold FValueComp.lt Nat.instFValueCompProd at h
+            simp at h
+            apply Prod.lex_iff.mp at h
+            cases h
+            case inl h =>
+              apply le_of_lt
+              exact h
+            case inr h =>
+              apply le_of_eq
+              exact h.1
 
 end NatGraph
