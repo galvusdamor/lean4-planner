2df5650edb2f74bb5fe1446c228e7cf3aeb9dbca
Galvus Damor <galvusdamor@gmail.com>
2026-04-03 18:17:28 +0200

    Added executable planner

--- Graphlib.lean
+++ Graphlib.lean
@@ -10,3 +10,16 @@ import Graphlib.Heuristics
 import Graphlib.Planner
 import Graphlib.AbstractionHeuristic
 import Graphlib.DeleteRelaxation
+
+import Validator.PlanningTask.Parser
+
+
+def main (args : List String) : IO Unit := do
+  match args with
+  | [] => IO.println "Usage: myexe <path>"
+  | path :: _ =>
+    let ⟨n, prob⟩ ← Validator.STRIPS.parse path
+    match Validator.planner prob (fun _ => 0) with
+    | some _ => IO.println "Solution found!"
+    | none   => IO.println "No solution found."
+
