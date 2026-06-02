f70b2848d9805d28973bddd8acd452fc20e373f8
Galvus Damor <galvusdamor@gmail.com>
2026-06-02 15:20:53 +0200

    Command line arguments for planner and plan output

--- Planner/Main.lean
+++ Planner/Main.lean
@@ -3,12 +3,30 @@ import planlib.H1
 
 import Validator.PlanningTask.Parser
 
+
+
+def pathToString {n : ℕ} {prob : Validator.STRIPS n} {cur goal : Validator.State n} (path : Validator.Path prob cur goal) : String := match path with
+    | .empty s => ""
+    | .cons a _ _ _ π => "(" ++ a.name ++ ")\n" ++ (pathToString π)
+
+def runPlanner (dom : String) (heur : String) : IO Unit := do
+    let ⟨n, prob⟩ ← Validator.STRIPS.parse dom -- What is `_n` here?
+    -- _n is the number of variables in the problem
+    let h : (Validator.State' n → ℕ) := match heur with
+    | "h1" => (fun s : Validator.State' n => Validator.h_1_new prob s)
+    | "h0" => (fun _ => 0)
+    | _ => (fun _ => 0)
+
+    match Validator.planner prob h with
+    | some plan =>
+      IO.println "Solution found!"
+      IO.println (pathToString plan.path)
+    | none   => IO.println "No solution found."
+
+
 def main (args : List String) : IO Unit := do
   match args with
   | [] => IO.println "Usage: myexe <path>"
-  | path :: _ =>
-    let ⟨_n, prob⟩ ← Validator.STRIPS.parse path -- What is `_n` here?
-    -- _n is the number of variables in the problem
-    match Validator.planner prob (fun s => Validator.h_1_new prob s) with
-    | some _ => IO.println "Solution found!"
-    | none   => IO.println "No solution found."
+  | path :: [] => runPlanner path "h1"
+  | path :: heur :: _ => runPlanner path heur
+
